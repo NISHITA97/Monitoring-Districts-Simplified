7c3e68c69d3f736dcb2261642c1ccc4130d9fc59
NISHITA97 <naggarwal97@gmail.com>
2018-03-12 02:09:04 +0530

    LoginUI added

--- app/src/main/java/com/example/android/monitoringdistrictssimplified/MainActivity.java
+++ app/src/main/java/com/example/android/monitoringdistrictssimplified/MainActivity.java
@@ -1,13 +1,15 @@
 package com.example.android.monitoringdistrictssimplified;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -24,6 +26,15 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
+            }
+        });
+
+        Button login_button = (Button) findViewById(R.id.login_button);
+        login_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent loginActivityIntent = new Intent(MainActivity.this,LoginActivity.class);
+                MainActivity.this.startActivity(loginActivityIntent);
             }
         });
     }
